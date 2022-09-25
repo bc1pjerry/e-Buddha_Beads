fd82035e73a07e4ccbe67190b2a637f81d7eed74
Ye Hangyu <32982019+HangyuYe@users.noreply.github.com>
2022-09-25 22:06:32 +0800

    Initial Commit

---
+++ e-Buddha_Beads/e_Buddha_BeadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_Buddha_BeadsApp.swift
+//  e-Buddha_Beads
+//
+//  Created by Jerry on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct e_Buddha_BeadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
