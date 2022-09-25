43c0077dc2a90a1e64b544319d04c4f75a2e4a31
Ye Hangyu <32982019+HangyuYe@users.noreply.github.com>
2022-09-26 00:09:13 +0800

    Update ContentView.swift
    Add transmit

--- e-Buddha_Beads/ContentView.swift
+++ e-Buddha_Beads/ContentView.swift
@@ -8,50 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isScaleDown = false
-    @State var isMovingDown = false
+    @State var isTouchUpInside = false
+    @Namespace private var shapeTransition
     
     private func firstBead() -> some View {
         Circle()
             .frame(width: 80)
             .foregroundColor(Color(cgColor: CGColor(red: 245/255, green: 153/255, blue: 42/255, alpha: 1)))
             .offset(x: 0, y: -180)
+            .matchedGeometryEffect(id: "First", in: shapeTransition)
     }
     private func secondBead() -> some View {
         Circle()
             .frame(width: 100)
             .foregroundColor(Color(cgColor: CGColor(red: 235/255, green: 180/255, blue: 46/255, alpha: 1)))
             .offset(x: 0, y: -100)
+            .matchedGeometryEffect(id: "Second", in: shapeTransition)
     }
     private func lastSecondBead() -> some View {
         Circle()
             .frame(width: 80)
             .foregroundColor(Color(cgColor: CGColor(red: 245/255, green: 153/255, blue: 42/255, alpha: 1)))
             .offset(x: 0, y: 180)
+            .matchedGeometryEffect(id: "lastSecond", in: shapeTransition)
     }
     private func lastBead() -> some View {
         Circle()
             .frame(width: 100)
             .foregroundColor(Color(cgColor: CGColor(red: 235/255, green: 180/255, blue: 46/255, alpha: 1)))
             .offset(x: 0, y: 100)
+            .matchedGeometryEffect(id: "Last", in: shapeTransition)
     }
     private func mainBead() -> some View {
         Circle()
             .frame(width: 150)
             .foregroundColor(.orange)
-            .scaleEffect(isScaleDown ? 0.5 : 1)
+            .matchedGeometryEffect(id: "Main", in: shapeTransition)
     }
     
     var body: some View {
         ZStack {
-            firstBead()
-            secondBead()
-            lastSecondBead()
-            lastBead()
-            mainBead()
+            if isTouchUpInside {
+                mainBead()
+            } else {
+                firstBead()
+                secondBead()
+                lastSecondBead()
+                lastBead()
+                mainBead()
+            }
         }
         .onTapGesture {
-            isScaleDown.toggle()
+            withAnimation {
+                isTouchUpInside.toggle()
+            }
         }
     }
 }
